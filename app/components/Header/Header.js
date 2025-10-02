71a78d4dccd118e76a7b1837021b9d2e481b98eb
MatteoCigognini <cigognini.matteo@gmail.com>
2025-10-02 13:09:09 +0200

    Aggiunto il mobile di navigazione da responsive

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -6,8 +6,9 @@ import styles from './Header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import Button from '../Buttons/Button'
-import { faBars, faMessage } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faMessage, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react'
+import { createPortal } from 'react-dom';
 
 export default function Header({ lang, dict, activeLink = "home" }) {
     const [isMobile, setIsMobile] = useState(false);
@@ -24,24 +25,24 @@ export default function Header({ lang, dict, activeLink = "home" }) {
     }, []);
 
     const handleMobileMenuButtonClick = () => {
-
+        console.log('jawidjwi');
+        setIsMenuMobileOpen(prev => !prev);
     }
 
     return <>
         <header className={styles.header}>
             <Breakpoint>
                 <div className={styles.headerContainer}>
-                    <button className={styles.mobileMenuButton}><FontAwesomeIcon icon={faBars} /></button>
+                    <button className={styles.mobileMenuButton} onClick={handleMobileMenuButtonClick}><FontAwesomeIcon icon={isMobileMenuOpen ? faXmark : faBars} /></button>
                     <h1 className={styles.title}>&lt;<strong>Cigo</strong>&gt;</h1>
                     <nav className={styles.nav}>
                         <Link href={`/${lang}`} className={`${styles.link} ${styles.noMobile} ${activeLink === 'home' ? styles.active : ''}`}>Home</Link>
                         <Link href={`/${lang}/projects`} className={`${styles.link} ${styles.noMobile} ${activeLink === 'projects' ? styles.active : ''}`}>{dict.projects.title}</Link>
                         <Link href={`/${lang}/blog`} className={`${styles.link} ${styles.noMobile} ${activeLink === 'blog' ? styles.active : ''}`}>Blog</Link>
-                        {!isMobile && <Button variant='primary' onClick={handleMobileMenuButtonClick}>{dict.contactMe}</Button>}
-                        {isMobile && <Button variant='primary' style={{ height: '50px', width: '50px', padding: '0' }}><FontAwesomeIcon icon={faMessage} /></Button>}
+                        <Button variant='primary'>{dict.contactMe}</Button>
 
                         {/* Social */}
-                        <div className={styles.socials}>
+                        <div className={`${styles.socials} ${styles.noMobile}`}>
                             <Link href={`https://github.com/MatteoCigognini`} target='_blank' className={styles.socialIcon}>
                                 <FontAwesomeIcon icon={faGithub} />
                             </Link>
@@ -59,5 +60,35 @@ export default function Header({ lang, dict, activeLink = "home" }) {
                 </div>
             </Breakpoint>
         </header>
+
+        {isMobileMenuOpen && createPortal(<div className={styles.backdrop}>
+            <div className={styles.mobileMenu}>
+                <h1 className={styles.title}>&lt;<strong>Cigo</strong>&gt;</h1>
+
+                {/* Navigazione principale */}
+                <nav className={styles.mobileNav}>
+                    <Link href={`/${lang}`} className={`${styles.link} ${activeLink === 'home' ? styles.active : ''}`}>Home</Link>
+                    <Link href={`/${lang}/projects`} className={`${styles.link} ${activeLink === 'projects' ? styles.active : ''}`}>{dict.projects.title}</Link>
+                    <Link href={`/${lang}/blog`} className={`${styles.link} ${activeLink === 'blog' ? styles.active : ''}`}>Blog</Link>
+                </nav>
+
+                {/* Socials */}
+                <div className={styles.socials}>
+                    <Link href={`https://github.com/MatteoCigognini`} target='_blank' className={styles.socialIcon}>
+                        <FontAwesomeIcon icon={faGithub} />
+                    </Link>
+                    <Link href={`https://www.facebook.com/cigognini.matteo`} target='_blank' className={styles.socialIcon}>
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </Link>
+                    <Link href={`https://www.linkedin.com/in/cigognini-matteo`} target='_blank' className={styles.socialIcon}>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </Link>
+                    <Link href={`https://www.instagram.com/cigognini.matteo`} target='_blank' className={styles.socialIcon}>
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </Link>
+                </div>
+            </div>
+            <div className={styles.closingZone} onClick={() => setIsMenuMobileOpen(false)}></div>
+        </div>, document.body)}
     </>
 }
